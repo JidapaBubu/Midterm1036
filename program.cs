2d6204687253c462b17efec67fbd2efe554bf2ac
JidapaBubu <jidapa4499@gmail.com>
2022-10-13 14:46:33 +0700

    Create noLoginMenu
    Create for user that didn't login

---
+++ program.cs
@@ -0,0 +1,16 @@
+class program{
+    static void Main(string args[]){
+        PrintMenuSceen();
+    }
+    static void PrintMenuSceen(){
+        noLogiMenu();
+    }
+    public static void noLogiMenu(){
+        Console.Write("Welcome Idia camp 2022 project registration system");
+        Console.Write("**************************************************");
+        Console.Write("1 for Register");
+        Console.Write("2 for Show attendees");
+        Console.Write("3 for Login");
+        Console.Write("**************************************************");
+    }
+}
