2d6856a943cf84bc3adb8acacd3f274a045f79a9
JidapaBubu <jidapa4499@gmail.com>
2022-10-13 15:30:17 +0700

    Update loginMenu

--- program.cs
+++ program.cs
@@ -1,16 +1,26 @@
 class program{
-    static void Main(string args[]){
+    static void Main(string[]args){
         PrintMenuSceen();
     }
     static void PrintMenuSceen(){
         noLogiMenu();
     }
     public static void noLogiMenu(){
-        Console.Write("Welcome Idia camp 2022 project registration system");
+        Console.Clear();
+        Console.Write("Welcome to Idia CAMP 2022 registration system");
         Console.Write("**************************************************");
         Console.Write("1 for Register");
         Console.Write("2 for Show attendees");
         Console.Write("3 for Login");
         Console.Write("**************************************************");
     }
+     public static void LoginMenu(){
+        Console.Clear();
+        Console.Write("Welcome to Idia CAMP 2022");
+        Console.Write("1 for Register");
+        Console.Write("2 for show all current students");
+        Console.Write("3 for show students");
+        Console.Write("3 for show teachers");
+    }
+   
 }
